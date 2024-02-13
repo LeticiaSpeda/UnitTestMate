a85401de5a394d4971843be6582adb69f2825afa
Leticia <leticiaspeda@gmail.com>
2024-02-13 10:16:01 -0300

    feat: criado test para verificar se uma string e um inteiro utilizando TDD

--- UnitTestMate/Utils.swift
+++ UnitTestMate/Utils.swift
@@ -4,4 +4,8 @@ final class Utils {
     static func avarege(numbers: [Int]) -> Int {
         return numbers.reduce(0, +) / numbers.count
     }
+    
+    static func isInt(text: String) -> Bool {
+        return Int(text) != nil
+    }
 }
--- UnitTestMateTests/UtilsTests.swift
+++ UnitTestMateTests/UtilsTests.swift
@@ -10,4 +10,14 @@ final class UtilsTests: XCTestCase {
         
         XCTAssertEqual(result, expected)
     }
+    
+    func testIsString_resultInInt() {
+        let textTrue = "1000"
+        let resultTrue = Utils.isInt(text: textTrue)
+        let textFalse = "100Text"
+        let resultFalse = Utils.isInt(text: textFalse)
+        
+        XCTAssertTrue(resultTrue)
+        XCTAssertFalse(resultFalse)
+    }
 }
